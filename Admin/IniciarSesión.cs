bbd28fa0a58dd368e1ab45cfba6294e76355814a
LourdesRod <lourd.rodoj@gmail.com>
2023-08-28 09:44:16 -0300

    se agrega usuario admin

---
+++ Admin/IniciarSesión.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaborACielo.Admin
+{
+    public partial class IniciarSesión : Form
+    {
+        public IniciarSesión()
+        {
+            InitializeComponent();
+        }
+    }
+}
